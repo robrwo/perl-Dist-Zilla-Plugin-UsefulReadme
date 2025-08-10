7bc683cd53a6ba36952777fcf4721b0f30dcdfdf
Robert Rothenberg <rrwo@cpan.org>
2025-08-10 11:42:05 +0100

    Commit Changes and bump $VERSION

--- lib/Dist/Zilla/Plugin/UsefulReadme.pm
+++ lib/Dist/Zilla/Plugin/UsefulReadme.pm
@@ -34,7 +34,7 @@ use experimental qw( lexical_subs postderef signatures );
 
 use namespace::autoclean;
 
-our $VERSION = 'v0.5.0';
+our $VERSION = 'v0.5.1';
 
 sub mvp_multivalue_args { qw( regions sections ) }
 
--- lib/Pod/Weaver/Section/InstallationInstructions.pm
+++ lib/Pod/Weaver/Section/InstallationInstructions.pm
@@ -21,7 +21,7 @@ use experimental qw( lexical_subs postderef signatures );
 
 use namespace::autoclean;
 
-our $VERSION = 'v0.5.0';
+our $VERSION = 'v0.5.1';
 
 =head1 SYNOPSIS
 
